da95c8347ff2cf5101c5c44786fe9881732628f9
Josephine2702 <irairaboch@gmail.com>
2023-03-05 14:37:30 +0600

    finish

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,8 +3,9 @@ const modals = () => {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
-            windows = document.querySelectorAll('[data-modal]');
-        
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll();
+
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
                 if (e.target) {
@@ -17,16 +18,20 @@ const modals = () => {
 
                 modal.style.display = "block";
                 document.body.style.overflow = "hidden";
+                document.body.style.marginRight = `${scroll}px`
             })
         });
 
     close.addEventListener('click', () => {
-        modal.style.display = "none";
-        document.body.style.overflow = "";
-
-        windows.forEach(item => {
+         windows.forEach(item => {
             item.style.display = 'none';
         });
+        
+        modal.style.display = "none";
+        document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`
+
+       
     });
 
     modal.addEventListener('click', (e) => {
@@ -37,6 +42,7 @@ const modals = () => {
 
         modal.style.display = "none";
         document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`
     }
     });
     }
@@ -48,7 +54,23 @@ const modals = () => {
         }, time);
     }
 
-        bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_close');
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+
+    }
+
+        bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
         bindModal('.phone_link', '.popup', '.popup_close');
         bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
         bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
